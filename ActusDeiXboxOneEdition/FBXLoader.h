4e14452c82e6f44188266f1e2d7a0e3ab63a60c7
Moises Carmona <MCarmona@student.fullsail.edu>
2020-05-18 21:08:07 -0400

    Its just as it is a loader nothing else, Export functionality removed

--- ActusDeiXboxOneEdition/FBXLoader.h
+++ ActusDeiXboxOneEdition/FBXLoader.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "pch.h"
-#include <fbxsdk.h>
 
 using namespace std;
 using namespace DirectX;
@@ -27,40 +26,11 @@ struct SimpleMesh
 	vector<int> indicesList;
 };
 // Simple material definition
-struct material_t
-{
-public:
-	enum e_component { EMISSIVE = 0, DIFFUSE, SPECULAR, SHININESS, COUNT };
-	struct component_t
-	{
-		float value[3] = { 0.0f, 0.0f, 0.0f };
-		float factor = 0.0f;
-		int64_t input = -1;
-	};
-	component_t& operator[](int i) { return components[i]; }
-	const component_t& operator[](int i)const { return components[i]; }
-private:
-	component_t components[COUNT];
-};
 
 
 
-vector<XMFLOAT3> positionList;
-vector<XMFLOAT3> normalList;
-vector<XMFLOAT3> uvList;
-
-vector< int > positionIndices, uvIndices, normalIndices;
 
 
-
-std::vector<file_path_t> paths;
-
-
-
-void FBX_InitLoad(const char* fbxfile, const char* meshfile, const char* matPath = "", const char* matfile = "");
-void ProcessFbxMesh(FbxNode* Node, const char* meshfile, const char* matpath = "", const char* matfile = "");
-void Compactify(SimpleVertex* verticesCompact, const char* meshfile);
-void ComputeTangent(SimpleMesh& simpleMesh);
 
 void Load_FBX(const char* meshFileName, SimpleMesh& mesh);
 
