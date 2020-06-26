af93fa0022231e6b8e19efc6a2a87e710392d335
Daniel Hayward <DKHayward@student.fullsail.edu>
2020-06-25 23:12:33 -0400

    AIUpdate before I pull master

--- ActusDeiXboxOneEdition/ADAI.cpp
+++ ActusDeiXboxOneEdition/ADAI.cpp
@@ -11,10 +11,18 @@ void ADAI::IdleState::Update(float _deltaTime)
 
 void ADAI::WayPointState::Update(float _deltaTime)
 {
+	if (FindNearest(owner->owner, objectsToAvoid, 50, 0) != nullptr)
+	{
+		owner->currentState = owner->states[1];
+	}
 }
 
 void ADAI::FlockingState::Update(float _deltaTime)
 {
+	if (FindNearest(owner->owner, objectsToAvoid, 50, 0) != nullptr)
+	{
+		owner->currentState = owner->states[1];
+	}
 	owner->owner->Velocity = VelocityWhenFlocking;
 	owner->owner->AddToPositionVector((XMFLOAT3&)owner->owner->Velocity);
 }
